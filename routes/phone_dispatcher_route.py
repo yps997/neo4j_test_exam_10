dc83cfddcb64f2b1d77924507ffb7842bbb66526
yps997 <yairps997@gmail.com>
2024-11-28 13:28:03 +0200

    add phone_dispatcher_route.py file with create endpoint

--- routes/phone_dispatcher_route.py
+++ routes/phone_dispatcher_route.py
@@ -1,9 +1,20 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, current_app
+
+from repo.phone_dispatcher_repo import Neo4jConnection
+
+# db = Neo4jConnection()
 
 phone_blueprint = Blueprint('phone_blueprint', __name__, url_prefix='/api/phone_tracker/')
 
 
 @phone_blueprint.route('/', methods=['POST'])
 def get_interaction():
-   print(request.json)
-   return jsonify({ }), 200
+   interaction_data = request.get_json()
+   print(interaction_data)
+   repo = Neo4jConnection(current_app.config['NEO4J_DRIVER'])
+   interaction_id = (repo.create_interaction(interaction_data))
+   return jsonify({"interaction_id": interaction_id}), 201
+
+
+
+
